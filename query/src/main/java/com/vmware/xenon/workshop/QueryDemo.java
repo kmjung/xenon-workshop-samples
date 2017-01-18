1b7361e3ae5f7b73e8fa19756eedd493ef9c1b04
Kenneth Jung <jungk@vmware.com>
2017-01-18 09:18:37 -0800

    Update query example code

--- query/src/main/java/com/vmware/xenon/workshop/QueryDemo.java
+++ query/src/main/java/com/vmware/xenon/workshop/QueryDemo.java
@@ -1,5 +1,8 @@
 package com.vmware.xenon.workshop;
 
+import java.util.HashMap;
+
+import com.vmware.xenon.common.Operation;
 import com.vmware.xenon.common.ServiceHost;
 import com.vmware.xenon.services.common.ExampleService;
 
@@ -25,6 +28,50 @@ public class QueryDemo {
         }));
         host.initialize(args);
         host.start();
-    }
 
+        host.registerForServiceAvailability((o, e) -> {
+            ExampleService.ExampleServiceState state = new ExampleService.ExampleServiceState();
+            state.counter = 1L;
+            state.documentSelfLink = "a";
+            state.keyValues = new HashMap<>();
+            state.keyValues.put("key1", "value1");
+            state.name = "Amanda";
+            state.sortedCounter = 1L;
+
+            Operation
+                    .createPost(host, ExampleService.FACTORY_LINK)
+                    .setBody(state)
+                    .setReferer("localhost")
+                    .sendWith(host);
+
+            state = new ExampleService.ExampleServiceState();
+            state.counter = 10L;
+            state.documentSelfLink = "b";
+            state.keyValues = new HashMap<>();
+            state.keyValues.put("key2", "value1");
+            state.name = "Bernard";
+            state.sortedCounter = 10L;
+
+            Operation
+                    .createPost(host, ExampleService.FACTORY_LINK)
+                    .setBody(state)
+                    .setReferer("localhost")
+                    .sendWith(host);
+
+            state = new ExampleService.ExampleServiceState();
+            state.counter = 100L;
+            state.documentSelfLink = "c";
+            state.keyValues = new HashMap<>();
+            state.keyValues.put("key1", "value3");
+            state.name = "Commander Adama";
+            state.sortedCounter = 100L;
+
+            Operation
+                    .createPost(host, ExampleService.FACTORY_LINK)
+                    .setBody(state)
+                    .setReferer("localhost")
+                    .sendWith(host);
+
+        }, ExampleService.FACTORY_LINK);
+    }
 }
